e7874d563bd3d5a448154f89fbd50cc6f0aea167
Seeta00 <seeta63966@gmail.com>
2024-11-22 18:48:03 +0530

    initial changes

---
+++ DataTypes.js
@@ -0,0 +1,28 @@
+//Conversion of data type
+/*const numstr = "36"   
+let num = Number(numstr)  //data types in conversion starts with caps letter
+
+console.log(typeof numstr);
+console.log(typeof num);*/
+//null --> num=0
+//"36abc"  --> will be converted to  umber but its value will be 'NaN'
+
+/*console.log(1 + "2");  
+console.log("1" + 2);   
+console.log(1 + 2 + "3");  //first num matters in all the cases
+console.log("1" + 2 + 3);
+
+console.log(+true); //writtens numeric value of the bool --> 1
+console.log(+"");                                   //   --> 0  */
+
+/*console.log(null <= 0);  //true
+console.log(null >= 0);  //true
+console.log(null == 0);  //false
+
+console.log(undefined <= 0);    //fasle
+console.log(undefined >= 0);    //false
+console.log(undefined == 0);    //false   */
+
+//console.log(2 === 2);  //for strictly equal content and data type must be same
+
+
---
+++ Strings.js
@@ -0,0 +1,6 @@
+//if string is used then we can use different properties based on r requirements 
+
+const name = new String("Siri")
+console.log(name[2]);
+console.log(name.__proto__);
+
