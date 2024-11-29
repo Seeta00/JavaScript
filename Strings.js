d2062b838ac33d89bb536deca4321891216b90e1
Seeta00 <seeta63966@gmail.com>
2024-11-29 16:02:16 +0530

    code updated

--- Strings.js
+++ Strings.js
@@ -4,8 +4,23 @@ const name = new String("Siri Adusumalli")
 /* console.log(name[2]);
 console.log(name.__proto__);
 console.log(name.substring(0, 6)); */
-console.log(name.substring(-8, 4));
-console.log(name.slice(-8,4)) // negative inputs don't work
+//console.log(name.substring(-8, 4));   //negative values are ignored and the count starts from zero
+const newName = name.slice(-8, 4)
+//console.log(newName);
 
+const name1 = "    Siri    "
+//console.log(name1.trim());    //extra spaces in the input are eliminated
+
+const url = "https://www.iare.ac.in/sites/default/files/BT23/ACSD02.pdf"
+// console.log(url.replace("i", "-"));
+// console.log(url.includes("sites"));
+
+
+const str = "The best is worth waiting"
+//console.log(str.split(' '))
+const words = str.split(" ")
+//console.log(words[3]);
+const chars = str.split('')
+//console.log(chars[10]);
 
 
