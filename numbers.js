6cd36b71e3a8719d3b2c0a5bcbd5ce3370431379
Seeta00 <seeta63966@gmail.com>
2024-11-29 23:24:47 +0530

    code updated

--- numbers.js
+++ numbers.js
@@ -1,4 +1,23 @@
 const num = new Number(100)
-console.log(num.toString().length)  //since now it is a str type, str features can be used
-console.log(num.toFixed(2));   //output: 100.00
+//console.log(num.toString().length)  //since now it is a str type, str features can be used
+//console.log(num.toFixed(2));   //output: 100.00
+
+const num1 = 12.936
+//console.log(num1.toPrecision(3));   //rounds off thenumber to nearest given value
+
+const currency = 100000
+//console.log(currency.toLocaleString('en-IN'));  //by default it shows US currency format
+
+
+// MATHS:
+//console.log(Math.abs(-4));  //absolute converts negative nums to positive
+//console.log(Math.round(59.6));
+// console.log(Math.ceil(4.2));  //top value is printed
+// console.log(Math.floor(4.2));  //lower value is printed
+// console.log(Math.min(1,2,3,4,5,6));
+// console.log(Math.max(1,2,3,4,5,6));
+// console.log(Math.random());  //the values are btw 0 and 1
+// console.log(Math.ceil(Math.random()));  //float val is rounded off
+
+
 
